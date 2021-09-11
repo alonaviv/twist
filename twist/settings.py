9b9a18f34e308ed590bbd9a38607c5fbeb9a5681
Alon Aviv <alona@infinidat.com>
2021-09-11 15:02:20 +0300

    Set up db

--- twist/settings.py
+++ twist/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'song_signup.apps.SongSignupConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'twist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twist_db',
+        'USER': 'postgres',
+        'PASSWORD': '76697421',
+        'HOST': 'localhost'
     }
 }
 
