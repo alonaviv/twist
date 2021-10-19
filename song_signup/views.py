424fd7adba136eac9dd56ba10ad483f530347373
Alon Aviv <alona@infinidat.com>
2021-10-19 21:00:24 +0300

    Bug fix: Got next songs of singers ordered by latest request time instead of earliest

--- song_signup/views.py
+++ song_signup/views.py
@@ -41,7 +41,7 @@ def get_how_long_waiting(singer):
 
 
 def get_singers_next_songs(singer):
-    return list(get_all_songs(singer).filter(performance_time=None).order_by('-request_time'))
+    return list(get_all_songs(singer).filter(performance_time=None).order_by('request_time'))
 
 
 def calculate_singer_priority(singer):
