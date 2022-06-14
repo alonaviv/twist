7dc7abba944b6b460a524cf594625947486ee356
Joey Geralnik <jgeralnik@gmail.com>
2022-06-14 23:57:05 +0300

    Add tests for algorithm

--- song_signup/migrations/0001_initial.py
+++ song_signup/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL('CREATE EXTENSION IF NOT EXISTS citext'),
         migrations.CreateModel(
             name='Singer',
             fields=[
