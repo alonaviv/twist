2f0aaf7073f76ac2bef558a44729df7a66386dbe
Alon Aviv <alona@infinidat.com>
2022-11-20 20:40:27 +0200

    Change front page css

--- song_signup/static/js/login.js
+++ song_signup/static/js/login.js
@@ -3,7 +3,7 @@ const formMessages = document.getElementById("form-messages");
 
 loginForm.addEventListener("submit", e => {
     e.preventDefault();
-     if (confirm("Did you pay the enterance fee?")) {
+     if (confirm("Did you pay the entrance fee?")) {
         const formData = new FormData(loginForm);
         fetch("/login", { method: "POST", body: formData })
             .then(async response => {
