be74702d5dc208e2ed538c118b28f740903b8358
Alon Aviv <alona@infinidat.com>
2022-11-21 00:02:22 +0200

    Remove confirm message on login

--- song_signup/static/js/login.js
+++ song_signup/static/js/login.js
@@ -3,18 +3,16 @@ const formMessages = document.getElementById("form-messages");
 
 loginForm.addEventListener("submit", e => {
     e.preventDefault();
-     if (confirm("Did you pay the entrance fee?")) {
-        const formData = new FormData(loginForm);
-        fetch("/login", { method: "POST", body: formData })
-            .then(async response => {
-                if (!response.ok) {
-                    const data = await response.json();
-                    throw Error(data.error);
-                }
-                window.location.replace("/home");
-            })
-            .catch(error => {
-                formMessages.innerHTML = `<p>${error.message}</p>`;
-            });
-     }
+    const formData = new FormData(loginForm);
+    fetch("/login", { method: "POST", body: formData })
+        .then(async response => {
+            if (!response.ok) {
+                const data = await response.json();
+                throw Error(data.error);
+            }
+            window.location.replace("/home");
+        })
+        .catch(error => {
+            formMessages.innerHTML = `<p>${error.message}</p>`;
+        });
 });
