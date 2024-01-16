95888ddb4cf9dd17e18e3a0c29da8ff5606fd379
Alon Aviv <alona@infinidat.com>
2024-01-16 23:09:17 +0200

    Remove STATICDIR_FILES - not using static dirs outside the apps

--- twist/settings.py
+++ twist/settings.py
@@ -175,9 +175,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'twist/static')
-]
 
 CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
