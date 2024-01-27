2a6f2b0a5eab4595fc28d7b55bb52f4001ab45f1
Alon Aviv <alona@infinidat.com>
2024-01-27 23:43:05 +0200

    Add singer/audience toggle on login

--- song_signup/static/js/login.js
+++ song_signup/static/js/login.js
@@ -1,5 +1,8 @@
 const loginForm = document.getElementById("login-form");
+const audienceWrapper = document.getElementById("audience-wrapper")
 const formMessages = document.getElementById("form-messages");
+const toggleCheckbox = document.getElementById("ticket-type-toggle")
+const toggleSlider = document.querySelector(".toggle-slider")
 
 loginForm.addEventListener("submit", e => {
     e.preventDefault();
@@ -16,3 +19,22 @@ loginForm.addEventListener("submit", e => {
             formMessages.innerHTML = `<p>${error.message}</p>`;
         });
 });
+
+toggleCheckbox.addEventListener("change", e => {
+    if (e.target.checked) {  // Singer
+        const sliderWidth = getComputedStyle(document.documentElement).getPropertyValue('--slider-width')
+        toggleSlider.style.left = `calc(100% - ${sliderWidth}`;
+        loginForm.style.display = 'block';
+        setTimeout(() => {
+            loginForm.classList.add('active');
+        }, 10);
+        audienceWrapper.classList.remove('active');
+
+    }
+    else { // Audience
+        toggleSlider.style.left = '0';
+        loginForm.classList.remove('active');
+        audienceWrapper.classList.add('active');
+        setTimeout(() => loginForm.style.display = 'none', 30);
+    }
+})
