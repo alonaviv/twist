6b041645dd04a14b6316e8cb9cfc47c6ad99a316
Joey Geralnik <joey@hyprtech.io>
2024-05-09 22:33:47 +0300

    Remove unused variable

--- song_signup/tasks.py
+++ song_signup/tasks.py
@@ -14,7 +14,6 @@ from .models import GroupSongRequest, SongLyrics, SongRequest
 
 logger = getLogger(__name__)
 
-GENIUS_URL_FORMAT = re.compile("genius\.com\/.*-lyrics$")
 USER_AGENT = "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
 SEARCH_MKT = "en-US"
 
