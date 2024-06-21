8503248b02998208581ce7771b6a6521fc292076
Alon Aviv <alona@infinidat.com>
2024-06-21 14:00:50 +0300

    Start working on fixing tests

--- song_signup/tests/test_lyrics.py
+++ song_signup/tests/test_lyrics.py
@@ -7,15 +7,13 @@ MUSICAL = "Book of Mormon"
 
 class TestParsers(TestCase):
     # For live testing. Don't run this as automation - it charges you on Bing.
-    def test_get_lyrics(self):
+    def no_test_get_lyrics(self):
         parser = AllMusicalsParser()
 
         lyrics = list(parser.get_lyrics(SONG_NAME, MUSICAL))
-        3
 
-    def test_get_lyrics_genius(self):
+    def no_test_get_lyrics_genius(self):
         # Uses dedicated API
         parser = GenuisParser()
 
         lyrics = list(parser.get_lyrics(SONG_NAME, MUSICAL))
-        3
