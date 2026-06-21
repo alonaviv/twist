02e386fbfeb3d78065e9348853a02cbdb045e88c
Alon Aviv <alona@infinidat.com>
2026-06-21 12:23:20 +0000

    Change deploy script to work with ubuntu user on EC2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\033[0;31mThis will reset all config values. Re-add them now so People's Choice site will keep working.\033[0m"
-git pull
+sudo -u ubuntu git pull
 
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml build
