41baee38a8ce39276c4c0d85b034102d45c817b7
Alon Aviv <alona@infinidat.com>
2026-07-08 15:09:38 +0300

    Fix sync db script

--- sync-db.sh
+++ sync-db.sh
@@ -1,4 +1,4 @@
-rsync -avz --ignore-existing digital-ocean-twist:~/db_backups/complete_evenings/ db_backups/
-rsync -avz --ignore-existing  digital-ocean-twist:~/media/selfies/ media/selfies/
+rsync -avz --ignore-existing ssaw:~/db_backups/complete_evenings/ db_backups/
+rsync -avz --ignore-existing  ssaw:~/media/selfies/ media/selfies/
 
 
